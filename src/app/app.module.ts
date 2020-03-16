f58401e00305208d901025c939289b81229dff3e
Etec 244 <e244ti@outlook.com>
2020-03-16 14:58:33 -0300

    Arrumando umas coisas.
    - Reposição dos códigos faltantes;
    - Identificação dos erros;
    
    *TUDO OK*

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { CoreModule } from './core/core.module';
+import { CidadesModule } from './cidades/cidades.module';
+import { RouterModule, Routes } from '@angular/router';
+import { CidadesPesquisaComponent } from './cidades/cidades-pesquisa/cidades-pesquisa.component';
+import { CidadesService } from './cidades/cidades.service';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes : Routes = [
+{path: 'cidades', component: CidadesPesquisaComponent}
+
+
+]
 
 @NgModule({
   declarations: [
@@ -10,9 +22,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule,
+    CidadesModule
   ],
-  providers: [],
+  providers: [CidadesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
